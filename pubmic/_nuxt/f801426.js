4ae10c1d591ef4998b33e3a2ab2663ea59818ea4
John <john.mbiddulph@gmail.com>
2021-10-20 00:07:59 +0100

    built project to dist folder

--- pubmic/_nuxt/f801426.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{601:function(n,e,t){"use strict";t.r(e);var o={name:"Notification",props:["message"]},c=t(85),component=Object(c.a)(o,(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",{staticClass:"notification is-danger"},[n._v("\n  "+n._s(n.message)+"\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
