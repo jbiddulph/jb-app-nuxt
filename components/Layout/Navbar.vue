665c64ea24b4fac4ff31e16f3ed2c96fab34ac80
John <john.mbiddulph@gmail.com>
2021-10-20 20:23:46 +0100

    and again

--- components/Layout/Navbar.vue
+++ components/Layout/Navbar.vue
@@ -15,52 +15,51 @@
       <div id="navMenu" class="navbar-menu" :class="{ 'is-active': showNav }">
         <div class="navbar-start">
           <div v-if="!isAuthenticated" class="main-menu">
-            <nuxt-link class="navbar-item" to="/">Home</nuxt-link>
-            <nuxt-link class="navbar-item" to="/events">Events</nuxt-link>
-            <nuxt-link class="navbar-item" to="/venues">Venues</nuxt-link>
+            <nuxt-link class="navbar-item is-size-4 has-text-centered" to="/">Home</nuxt-link>
+            <nuxt-link class="navbar-item is-size-4 has-text-centered" to="/events">Events</nuxt-link>
+            <nuxt-link class="navbar-item is-size-4 has-text-centered" to="/venues">Venues</nuxt-link>
           </div>
 
           <div v-if="isAuthenticated" class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
+            <a class="navbar-link is-size-4">
               Events
             </a>
             <div class="navbar-dropdown">
-              <nuxt-link class="navbar-item" to="/admin/events/add">Add Event</nuxt-link>
-              <nuxt-link class="navbar-item" to="/admin/events">Events</nuxt-link>
+              <nuxt-link class="navbar-item is-size-5 has-text-centered" to="/admin/events/add">Add Event</nuxt-link>
+              <nuxt-link class="navbar-item is-size-5 has-text-centered" to="/admin/events">Events</nuxt-link>
             </div>
           </div>
           <div v-if="isAuthenticated" class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
+            <a class="navbar-link is-size-4">
               Venues
             </a>
             <div class="navbar-dropdown">
-              <nuxt-link class="navbar-item" to="/admin/venues/add">Add Venue</nuxt-link>
-              <nuxt-link class="navbar-item" to="/admin/venues">Venues</nuxt-link>
+              <nuxt-link class="navbar-item is-size-5 has-text-centered" to="/admin/venues/add">Add Venue</nuxt-link>
+              <nuxt-link class="navbar-item is-size-5 has-text-centered" to="/admin/venues">Venues</nuxt-link>
             </div>
           </div>
           <div v-if="isAuthenticated" class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
+            <a class="navbar-link is-size-4">
               Users
             </a>
             <div class="navbar-dropdown">
-              <nuxt-link class="navbar-item" to="/admin/users/add">Add User</nuxt-link>
-              <nuxt-link class="navbar-item" to="/admin/users">Users</nuxt-link>
+              <nuxt-link class="navbar-item is-size-5 has-text-centered" to="/admin/users/add">Add User</nuxt-link>
+              <nuxt-link class="navbar-item is-size-5 has-text-centered" to="/admin/users">Users</nuxt-link>
             </div>
           </div>
         </div>
 
         <div class="navbar-end">
           <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-              Admin
-            </a>
             <div v-if="!isAuthenticated" class="navbar-dropdown">
-              <nuxt-link class="navbar-item" to="/auth/register">Register</nuxt-link>
-              <nuxt-link class="navbar-item" to="/auth/login">Log In</nuxt-link>
+              <!-- <nuxt-link class="navbar-item" to="/auth/register">Register</nuxt-link>
+              <nuxt-link class="navbar-item" to="/auth/login">Log In</nuxt-link> -->
             </div>
             <div v-else class="navbar-dropdown">
-              <nuxt-link class="navbar-item" to="/auth/user">{{ loggedInUser.name }}</nuxt-link>
-              <a class="navbar-item has-text-danger" @click="logout">Logout</a>
+              <div class="columns is-mobile">
+                <nuxt-link class="navbar-item column is-size-6 has-text-centered" to="/auth/user">{{ loggedInUser.name }}</nuxt-link>
+                <a class="navbar-item has-text-danger column is-size-6 has-text-centered" @click="logout">Logout</a>
+              </div>
             </div>
           </div>
           <!-- <div class="navbar-item">
